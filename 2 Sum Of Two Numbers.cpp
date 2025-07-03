e4cb7711290d3f31f23a9eda1c6585ff4cdd0a4b
Nayan <nayanmahipal4141@gmail.com>
2025-07-03 16:09:22 +0530

    Create 2 Sum Of Two Numbers.cpp

---
+++ 2 Sum Of Two Numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, sum;
+    cout << "Enter Number 1: ";
+    cin >> A;
+    cout << "Enter Number 2: ";
+    cin >> B;
+    sum = A + B;
+    cout << "Sum is: " << sum << endl;
+    return 0;
+}
