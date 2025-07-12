51c567951bdc68e428a02ad420885dea22cf3cf2
Nayan <nayanmahipal4141@gmail.com>
2025-07-12 10:13:40 +0530

    Update MIN MAX Num.cpp

--- UNIT 1 Exercise/MIN MAX Num.cpp
+++ UNIT 1 Exercise/MIN MAX Num.cpp
@@ -3,26 +3,30 @@
 using namespace std;
 
 int main() {
-    int a, b, c;
+    int a, b, c, d, e;
     int min_num, max_num;
     
     cout << "Enter three numbers: ";
-    cin >> a >> b >> c;
+    cin >> a >> b >> c >> d >> e;
   
     if (a <= b && a <= c) {
         min_num = a;
-    } else if (b <= a && b <= c) {
+    } else if (b <= a && b <= d) {
         min_num = b;
-    } else {
+    } else if (c <= d && c <= e) {
         min_num = c;
     }
+    
+    else {
+        min_num = e;
+    }
 
-    if (a >= b && a >= c) {
+    if (a >= b && a >= e) {
         max_num = a;
-    } else if (b >= a && b >= c) {
+    } else if (b >= a && b >= e) {
         max_num = b;
     } else {
-        max_num = c;
+        max_num = e;
     }
     
     cout << "Minimum number is: " << min_num << endl;
