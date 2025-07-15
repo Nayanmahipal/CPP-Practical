753ab6f14c4aa07ec6d19d65325d2f246f154fa2
Aayushi <mananiaayushi@gmail.com>
2025-07-15 09:06:43 +0530

    Create Calculator 1.h

---
+++ Calculator 1.h
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int add(int a,int b){
+return a+b;
+}
+int subtract(int a,int b){
+return a-b;
+}
+
+int multiply(int a,int b){
+return a*b;
+}
+
+int division(int a,int b){
+return a/b;
+}
